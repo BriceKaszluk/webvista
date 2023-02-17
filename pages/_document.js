90e3d372ac25dfdedf67ea5ce4d8d03605400ee3
BriceKaszluk <kaszlukb@gmail.com>
2023-02-17 11:11:59 +0100

    fix

--- pages/_document.js
+++ pages/_document.js
@@ -3,6 +3,31 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
+              <title>
+          Création de sites web évolutifs pour les particuliers et les
+          entreprises
+        </title>
+        <link rel="canonical" href="https://webvista.fr/" key="index" />
+        <meta charSet="UTF-8" id="iq5ty" key="index" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" key="index" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+</meta>
+        <meta
+          name="description"
+          content="Faites confiance à un développeur freelance expérimenté pour créer un site web professionnel qui répond à vos besoins. Contactez-moi dès maintenant."
+          key="index"
+        />
+        <meta
+          name="keywords"
+          content="création de site web, développeur freelance, site vitrine, site professionnel, développement web"
+          key="index"
+        />
+        <meta name="robots" content="index, follow" key="index" />
+        <meta
+          property="og:image"
+          content="../assets/og-image.png"
+          key="index"
+        />
       <Head />
       <body>
         <Main />
