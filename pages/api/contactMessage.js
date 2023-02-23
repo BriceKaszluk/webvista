86e1e5bb8e0bf8a41d9e19f8989d5823b9feab0f
BriceKaszluk <kaszlukb@gmail.com>
2023-02-23 17:08:40 +0100

    fix contact message

--- pages/api/contactMessage.js
+++ pages/api/contactMessage.js
@@ -1,23 +1,23 @@
 export default async function sendEmail(req, res) {
-  const { fullName, email, mess } = req.body;
+  const { fullName, email, message } = req.body;
 
   // ... valider les données du formulaire et envoyer l'email
   try {
     const sgMail = require('@sendgrid/mail')
     sgMail.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY)
 
-    const message = {
+    const messageToSend = {
       to: process.env.NEXT_PUBLIC_SENDGRID_SENDER,
       from: process.env.NEXT_PUBLIC_SENDGRID_SENDER,
       subject: `Nouveau message de ${fullName}`,
-      text: `Nouveau message de ${fullName} (${email}), son message: ${mess}`,
+      text: `Nouveau message de ${fullName} (${email}), son message: ${message}`,
       html: `
         <p>Nouveau message de ${fullName} (${email}), son message:
-        <p>${mess}</p>
+        <p>${message}</p>
       `
     }
 
-    await sgMail.send(message)
+    await sgMail.send(messageToSend)
 
     res.status(200).json({ success: true })
   } catch (error) {
