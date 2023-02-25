715952a59b6a36a0f2affc8b8c463082e5ef1705
BriceKaszluk <kaszlukb@gmail.com>
2023-02-25 19:01:53 +0100

    feat(frontend):add hojar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import { hotjar } from 'react-hotjar'
+import { useEffect } from 'react'
 import "@/styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
+
+  useEffect(() => {
+    hotjar.initialize(3383799, 6)
+  }, [])
+
   return (
     <>
       <Component {...pageProps} />
